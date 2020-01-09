bb70d69f7370c49734fa4c7ff854fbadaab1ceae
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-01-09 18:04:19 -0500

    Rich app created

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Pedro Martinez on 1/9/20.
+//  Copyright © 2020 Pedro Martinez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
